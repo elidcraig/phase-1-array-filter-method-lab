19786acb2b3da2342cce8402fd5ae38a152274ba
Eli Craig <elidcraig@gmail.com>
2022-04-27 12:53:38 -0700

    commit tree 12a60a62f9919019922324666cc18c4d4ee37262

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// Code your solution here
+
+function findMatching(list, checkName) {
+    return list.filter(name => checkName.toLowerCase() === name.toLowerCase())
+}
+
+function fuzzyMatch(list, checkStart) {
+    return list.filter(name => checkStart.toLowerCase() === name.slice(0,2).toLowerCase())
+}
+
+function matchName(drivers, checkName) {
+    return drivers.filter(obj => obj['name'] === checkName)
+}
